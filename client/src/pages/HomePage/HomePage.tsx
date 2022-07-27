fee95bd01f073a312e8e20b71da5f50be8925318
TechnoGecko <cr5697@gmail.com>
2022-07-27 01:38:45 -0700

    small changes

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './HomePage.css';
 import IntroScreen from './IntroScreen/IntroScreen';
+import { createStyles } from '@mantine/core';
 
 type Props = {};
 
-
+const useStyles = createStyles((theme, _params, getRef) => ({
+  pageWrapper: {
+    height: '100vh',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}));
 
 const HomePage = (props: Props) => {
+  const { classes } = useStyles();
+
   return (
-    <div>
+    <div className={classes.pageWrapper}>
       HomePage
       <IntroScreen />
     </div>
--- client/src/pages/HomePage/IntroScreen/IntroScreen.tsx
+++ client/src/pages/HomePage/IntroScreen/IntroScreen.tsx
@@ -5,24 +5,27 @@ import { ClassNames } from '@emotion/react';
 type Props = {};
 
 const useStyles = createStyles((theme, _params, getRef) => ({
-container: {
-  backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-  minWidth: 250,
-  width: '90%',
-  height: 220,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  border: '1px solid white',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  borderRadius: theme.radius.sm,
-  [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-    [`& .${getRef('child')}`]: {
-      fontSize: theme.fontSizes.xs,
-      },
-    },
-  },
+  container: {
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[5]
+        : theme.colors.gray[1],
+    minWidth: 250,
+    width: '90%',
+    height: '85vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid white',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: theme.radius.sm,
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      [`& .${getRef('child')}`]: {
+        fontSize: theme.fontSizes.xs
+      }
+    }
+  }
 }));
 
 const IntroScreen = (props: Props) => {
